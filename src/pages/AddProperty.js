747a2c3a159d0a580069f078a7f50d3ec590dad8
pelumi454 <amoochristianah454@gmail.com>
2022-04-28 12:07:13 +0100

    fix: add input fields to add property page

--- src/pages/AddProperty.js
+++ src/pages/AddProperty.js
@@ -1,16 +1,33 @@
 import React from "react";
+// import { Input } from "../components/input";
 
 const AddProperty = () => {
-    return (
-        <div className="containere">
-            <div className="row">
-                 <div className="col-sm-12 col-md-12">
-                    <form action="" className="mb-0" id="add-property-form">
-                        <input type="text" />
-                    </form>
-                    </div>
+  return (
+    <div className="containere">
+      <div className="row">
+        <div className="col-sm-12 col-md-12">
+          <form action="" className="mb-0" id="add-property-form">
+            <div className="form-box">
+              <div className="row">
+                <div className="col-sm-12 col-md-12"></div>
+                <div className="col-sm-12 col-md-12">
+                  <div className="h1 mb-5">Add Property</div>
+                  <div className="form-group">
+                    <label for="">Property Address</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Property Name"
+                                      />
+                    {/* <p id="p1" style="color: red;"></p> */}
+                  </div>
+                </div>
+              </div>
             </div>
-     </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
