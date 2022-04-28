6556c285e683526c6b847d5e766f469445d44f0e
pelumi454 <amoochristianah454@gmail.com>
2022-04-28 10:44:04 +0100

    fix: create input component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <div>Navbar</div>
+//        <div>AddProperty</div>
+//       <nav class="navbar navbar-expand-lg default-layout__inner">
+//     <div class="container py-1" id="nav-container">
+//       <a href="./index.html" to="{RouteLinks.home}" class="navbar-brand">YourAgent
+//       </a>
+//       <i class="navbar-toggler bi bi-list" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+//         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"></i>
+//       <div class="collapse navbar-collapse justify-content-end
+//           id=" navbarNav">
+//         <ul class="navbar-nav d-flex justify-content-end mt-2 mb-lg-0">
+//           <li class="navigation__list-item py-1" key="{index}">
+//             <a href="./viewproperty.html" class="navigation-link py-1">
+//               View Property
+//             </a>
+//           </li>
+//           <a href="./addproperty.html">
+//             <button class="btn btn-primary">Add Property</button>
+//           </a>
+//         </ul>
+//       </div>
+//     </div>
+//   </nav>
+
+  )
+}
+
+export default Navbar
---
+++ src/pages/AddProperty.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AddProperty = () => {
+    return (
+        <div className="containere">
+            <div className="row">
+                 <div className="col-sm-12 col-md-12">
+                    <form action="" className="mb-0" id="add-property-form">
+                        <input type="text" />
+                    </form>
+                    </div>
+            </div>
+     </div>
+  );
+};
+
+export default AddProperty;
