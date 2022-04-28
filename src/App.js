c58bf3b984fbb783f29f2be11f4076b910dc4a90
pelumi454 <amoochristianah454@gmail.com>
2022-04-28 10:08:37 +0100

    fix: install dependencies

--- src/App.js
+++ src/App.js
@@ -23,3 +23,28 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+// import React from "react";
+// import AppRoutes from "./routes/AppRoutes";
+// // Redux
+// import { Provider } from "react-redux";
+// import store from "./store";
+
+
+// function App()
+// {
+//     return (
+//         <Provider store={store}>
+//             <div className="App">
+//                 <AppRoutes />
+//             </div>
+//         </Provider>
+//     );
+// }
+
+// export default App;
+
