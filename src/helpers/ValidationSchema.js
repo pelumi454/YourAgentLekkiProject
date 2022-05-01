66013e5002d21fbf48685f088e4471a57960b3f0
pelumi454 <amoochristianah454@gmail.com>
2022-05-02 00:16:23 +0100

    fix: general clean up

--- src/helpers/ValidationSchema.js
+++ src/helpers/ValidationSchema.js
@@ -4,6 +4,7 @@ const inputValueValidationSchema = Yup.object().shape({
   address: Yup.string().required("Address is required"),
   type: Yup.string().required("Property type is required"),
   bathroom: Yup.number().required("Number of bathrooms is required"),
+  bedroom: Yup.number().required("Number of bedrooms is required"),
   kitchen: Yup.number().required("Number of kitchens is required"),
   toilet: Yup.number().required("Number of Toilets is required"),
   sittingRoom: Yup.number().required("Number of sitting rooms is required"),
